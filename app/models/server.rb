9554fb2f897f9c20027d56350a78acbc182617f5
root <root@srv.painelfinanceiro.com>
2013-08-11 02:05:34 -0300

    somar visualizacoes total

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,13 @@
 class Server < ActiveRecord::Base
   has_many :email, dependent: :destroy
+  def total_visualizacoes(id)
+    server = Server.find(id)
+    @total = 0
+    server.email.each do |email|
+      unless email.visualizacao.nil?
+        @total += email.visualizacao
+      end
+    end
+    return @total
+  end
 end
